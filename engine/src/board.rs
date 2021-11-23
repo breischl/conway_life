5f78808d170d750e227eb6c6e67bbbdab24bc47e
BrianReischl <breischl@gmail.com>
2021-11-23 08:44:33 -0700

    Basic board functionality and refactor

--- engine/src/board.rs
+++ engine/src/board.rs
@@ -1,21 +1,16 @@
 mod board {
     use crate::LifePoint;
+    use std::convert::From;
     use std::fmt;
 
     #[derive(Clone, Debug)]
     pub struct LifeBoard {
-        live_points: Vec<LifePoint>,
+        grid: Vec<Vec<bool>>,
     }
 
     impl LifeBoard {
-        pub fn new(state: Vec<LifePoint>) -> LifeBoard {
-            LifeBoard { live_points: state }
-        }
-
         pub fn step_one(&self) -> LifeBoard {
-            LifeBoard {
-                live_points: vec![],
-            }
+            LifeBoard { grid: vec![] }
         }
 
         pub fn step_many(&self, num_steps: u64) -> LifeBoard {
@@ -30,6 +25,52 @@ mod board {
                 board
             }
         }
+
+        pub fn is_live(&self, x: usize, y: usize) -> bool {
+            self.grid
+                .get(x)
+                .and_then(|row| row.get(y))
+                .map(|b| b.clone())
+                .unwrap_or(false)
+        }
+
+        pub fn get_neighbors(&self, x: usize, y: usize) -> Vec<Vec<bool>> {
+            vec![
+                vec![
+                    self.is_live(x - 1, y - 1),
+                    self.is_live(x, y - 1),
+                    self.is_live(x + 1, y - 1),
+                ],
+                vec![
+                    self.is_live(x - 1, y),
+                    self.is_live(x, y),
+                    self.is_live(x + 1, y),
+                ],
+                vec![
+                    self.is_live(x - 1, y + 1),
+                    self.is_live(x, y + 1),
+                    self.is_live(x + 1, y + 1),
+                ],
+            ]
+        }
+
+        pub fn get_live_count(&self) -> u64 {
+            let mut count: u64 = 0;
+            for row in &self.grid {
+                for cell in row {
+                    if *cell {
+                        count = count + 1;
+                    }
+                }
+            }
+            count
+        }
+    }
+
+    impl From<Vec<Vec<bool>>> for LifeBoard {
+        fn from(points: Vec<Vec<bool>>) -> Self {
+            LifeBoard { grid: points }
+        }
     }
 
     #[cfg(test)]
@@ -38,18 +79,22 @@ mod board {
 
         #[test]
         pub fn can_create_empty_board() {
-            let board = LifeBoard::new(vec![]);
-            assert_eq!(board.live_points.len(), 0)
+            let board = LifeBoard::from(vec![]);
+            assert_eq!(0, board.get_live_count());
+            assert_eq!(false, board.is_live(0, 0));
+            assert_eq!(false, board.is_live(0, 1));
+            assert_eq!(false, board.is_live(1, 1));
         }
 
         #[test]
         pub fn can_create_filled_board() {
-            let points = vec![LifePoint { x: 0, y: 0 }, LifePoint { x: 1, y: 2 }];
-            let board = LifeBoard::new(points.clone());
-            assert_eq!(board.live_points.len(), 2);
-
-            let board_points = board.live_points;
-            assert_eq!(points[0], board_points[0])
+            let points = vec![vec![false, true], vec![true, false]];
+            let board = LifeBoard::from(points.clone());
+            assert_eq!(2, board.get_live_count());
+            assert_eq!(false, board.is_live(0, 0));
+            assert_eq!(true, board.is_live(0, 1));
+            assert_eq!(true, board.is_live(1, 0));
+            assert_eq!(false, board.is_live(1, 1));
         }
     }
 }
