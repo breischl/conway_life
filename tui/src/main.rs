24c12024d6c4efd344b23438c5242e9e8ed7e956
BrianReischl <breischl@gmail.com>
2021-11-27 14:57:31 -0700

    Adding a terminal UI project and moving to a workspace format

---
+++ tui/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use tui::backend::CrosstermBackend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::widgets::{Block, Borders, Widget};
+use tui::Terminal;
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.draw(|f| {
+       let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10)
+                ].as_ref()
+            )
+            .split(f.size());
+        let block = Block::default()
+             .title("Block")
+             .borders(Borders::ALL);
+        f.render_widget(block, chunks[0]);
+        let block = Block::default()
+             .title("Block 2")
+             .borders(Borders::ALL);
+        f.render_widget(block, chunks[1]);
+    })?;
+
+    Ok(())
+}
